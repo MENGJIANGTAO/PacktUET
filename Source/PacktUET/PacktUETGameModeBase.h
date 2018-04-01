e895bcc8473f7d8e1338c5f604e4d945f3890855
MENGJIANGTAO <meng_jiangtao@163.com>
2018-04-01 19:34:15 +0800

    InitProject.

---
+++ Source/PacktUET/PacktUETGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PacktUETGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PACKTUET_API APacktUETGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+};
