17abf7731bb306770443359a82c2f0f1a8d8d803
MENGJIANGTAO <meng_jiangtao@163.com>
2018-04-13 23:00:59 +0800

    SpawnActor In gameMode.
    SpawnActor In gameMode.

--- Source/PacktUET/PacktUETGameModeBase.cpp
+++ Source/PacktUET/PacktUETGameModeBase.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PacktUETGameModeBase.h"
-
+#include "TestActor.h"
 
 
 APacktUETGameModeBase::APacktUETGameModeBase(const FObjectInitializer& PCIP) : Super(PCIP)
@@ -18,4 +18,7 @@ APacktUETGameModeBase::APacktUETGameModeBase(const FObjectInitializer& PCIP) : S
 void APacktUETGameModeBase::BeginPlay()
 {
 	UE_LOG(LogTemp, Warning, TEXT("BeginPlayisDoing."));
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("This is an on screen message!"));
+	FTransform Location = FTransform();
+	ATestActor* T1 = (ATestActor*)GetWorld()->SpawnActor(ATestActor::StaticClass(), &Location);
 }
