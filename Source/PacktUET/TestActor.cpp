edfb3d63c06810f3e6a0cc47cc42b54fb5bf661b
MENGJIANGTAO <meng_jiangtao@163.com>
2018-04-15 23:21:11 +0800

    创建MESH添加root。

--- Source/PacktUET/TestActor.cpp
+++ Source/PacktUET/TestActor.cpp
@@ -1,14 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TestActor.h"
-
-
+#include "UObject/ConstructorHelpers.h"
+#include "Components/SphereComponent.h"
 // Sets default values
 ATestActor::ATestActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	USphereComponent* SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
+	RootComponent = SphereComponent;
+	SphereComponent->InitSphereRadius(40.0f);
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("BaseMeshComponent");
+	auto MeshAsset = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Game/Models/Cube.Cube'"));
+	if (MeshAsset.Object != nullptr)
+	{
+		Mesh->SetStaticMesh(MeshAsset.Object);
+	}
 }
 
 // Called when the game starts or when spawned
